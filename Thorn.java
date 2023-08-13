0fe452208a3d7c56c0f9fed4c54eef1fcfa82d5b
ThrowMax <135956348+ThrowMax@users.noreply.github.com>
2023-08-13 17:49:21 +0400

    Add files via upload

---
+++ Thorn.java
@@ -0,0 +1,10 @@
+package com.javarush.games.racer.road;
+
+import com.javarush.games.racer.road.RoadObject;
+
+public class Thorn extends RoadObject {
+    public Thorn(int x,int y){
+        super(RoadObjectType.THORN, x, y);
+        speed=0;
+    }
+}
